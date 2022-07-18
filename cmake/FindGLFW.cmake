252d578c68688da420424d04b2b07839b03d1649
Muhammad Labiyb Afakh <labiybafakh@gmail.com>
2022-07-19 08:06:28 +0900

    test

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -6,6 +6,6 @@ find_path(GLFW_INCLUDE_DIR glfw3.h
         PATHS ${FIND_GLFW_PATHS})
 
 find_library(GLFW_LIBRARY
-        NAMES glfw.3.4
+        NAMES glfw.so.3.4
         PATH_SUFFIXES lib
         PATHS ${FIND_GLFW_PATHS})
--- cmake/FindMujoco.cmake
+++ cmake/FindMujoco.cmake
@@ -6,6 +6,6 @@ find_path(MUJOCO_INCLUDE_DIR
         PATHS ${FIND_MUJOCO_PATHS})
 
 find_library(MUJOCO_LIBRARY
-        NAMES mujoco.2.2.0
+        NAMES mujoco.so.2.2.1
         PATH_SUFFIXES lib
         PATHS ${FIND_MUJOCO_PATHS})
